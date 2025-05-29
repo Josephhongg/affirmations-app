9e8f4393a33c596cf0e649db109696ecb4c2b250
josephhongg <joseph.hong99@gmail.com>
2025-05-29 18:41:41 +1200

    Edited Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,15 @@
 "use client";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-           <img src="/logo.svg" alt="logo" className={styles.logo}/> 
-           <h1 className={styles.title}>The Art of Optimism</h1>
-        </header>
-    )
+	return (
+		<header className={styles.header}>
+			<img
+				src="/logo.svg"
+				alt="The Art of Optimism logo"
+				className={styles.logo}
+			/>
+			<h1 className={styles.title}>The Art of Optimism</h1>
+		</header>
+	);
 }
