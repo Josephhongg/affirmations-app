f749ef649d91b723a3d68160aecb90638384d922
josephhongg <joseph.hong99@gmail.com>
2025-05-29 18:53:46 +1200

    Import components to page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Header from "../components/Header"
-import TextToSpeech from "../components/TextToSpeech"
+import Header from "../components/Header";
+import TextToSpeech from "../components/TextToSpeech";
 
 export default function Page() {
-  return (
-    <>
-    <Header />
-    <TextToSpeech />
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<TextToSpeech />
+		</>
+	);
 }
